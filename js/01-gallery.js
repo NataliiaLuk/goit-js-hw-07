fc24d6296d480191d14bfa5a9f7c652d26ae1c47
NataliiaLuk <lukashevych.nat@gmail.com>
2024-03-31 18:03:30 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,38 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+const gallery = document.querySelector('ul');
+
+const imgMarkup = createImgItemMarkup(galleryItems);
+gallery.innerHTML = imgMarkup;
+
+function createImgItemMarkup(items) {
+return items.map(({preview, original, description}) => {
+        return `<li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>`
+    }).join('');
+
+}
 console.log(galleryItems);
+
+gallery.addEventListener('click', onImgItemClick);
+
+function onImgItemClick(evt) {
+    evt.preventDefault();
+    const bigImg = evt.target.dataset.source;
+    const modalImgMarkup  = basicLightbox.create(`
+    <div class="modal">
+         <img src="${bigImg}" width="800" height="600">
+    </div>
+`)
+    modalImgMarkup.show()
+}
+
